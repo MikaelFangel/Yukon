a14df882a87dc2f3eb08ead333956c603a7ce5c6
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2022-04-04 15:31:43 +0200

    added empty view

---
+++ view.h
@@ -0,0 +1,49 @@
+//
+// Created by mikael on 04/04/2022.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int generateView(bool isEmpty);
+int generateColumns();
+int clearView();
+int printCommandConsole(char lastCommand[], char message[]);
+
+int generateView(bool isEmpty) {
+    clearView();
+    generateColumns();
+
+    if(isEmpty) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 0; j < 8; ++j) {
+                printf("\t");
+            }
+            printf("[]\tF%d\n", i);
+        }
+        printCommandConsole("", "");
+    }
+
+    return 0;
+}
+
+int generateColumns() {
+    for (int i = 1; i <= 7; ++i) {
+        printf("C%d\t", i);
+    }
+    printf("\n\n");
+    return 0;
+}
+
+int printCommandConsole(char lastCommand[], char message[]) {
+    printf("LAST Command: %s \n", lastCommand);
+    printf("Message: %s \n", message);
+    printf("INPUT > ");
+
+    return 0;
+}
+
+int clearView() {
+    system("clear");
+    return 0;
+}
