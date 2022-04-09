681cdab06c1afbbae045c315735cc28e893b1929
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2022-04-09 13:39:26 +0200

    added testing for adding to linkedlist

---
+++ test/main_test.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include "../linkedlist.h"
+
+void linkedListAdd();
+
+int main(void) {
+    linkedListAdd();
+
+    return 0;
+}
+
+void linkedListAdd() {
+    bool status = true;
+
+    Linked_list * list = createLinkedList();
+    addNode(list, "1");
+    addNode(list, "2");
+    addNode(list, "3");
+    addNode(list, "4");
+    addNode(list, "5");
+    addNode(list, "6");
+    addNode(list, "7");
+    addNode(list, "8");
+    addNode(list, "9");
+    addNode(list, "10");
+
+    if(strcmp(list->head->key, "1") != 0) {
+        printf("linkedListAdd Error! Wrong node at head");
+        status = false;
+    }
+    if(strcmp(list->tail->key, "10") != 0) {
+        printf("linkedListAdd Error! Wrong node at tail");
+        status = false;
+    }
+    if(list->size != 10) {
+        printf("linkedListAdd Error! Wrong size of list");
+        status = false;
+    }
+
+    if(status == true) {
+        puts("linkedListAdd: Test Passed!");
+    }
+}
