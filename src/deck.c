9ee0ecd90fb674a9ce60b76e74e2eb1708a41e35
mikael <mikael.fangel@pm.me>
2022-04-20 18:34:01 +0200

    added suits to cards

--- src/deck.c
+++ src/deck.c
@@ -10,18 +10,27 @@ Card *deck[4] = {clubs, diamonds, hearts, spades};
 
 // TODO Implement with linked list
 int fillSuits() {
+    char suits[4] = {'C', 'D', 'H','S'};
+
     for (int i = 0; i < 4; i++) {
         deck[i][0].value = 'A';
+        deck[i][0].suit = suits[i];
 
         for (int j = 1; j < 10; ++j) {
             deck[i][j].value = (j + 1) + '0';
+            deck[i][j].suit = suits[i];
         }
 
         deck[i][9].value = 'T';
+        deck[i][9].suit = suits[i];
         deck[i][10].value = 'J';
+        deck[i][10].suit = suits[i];
         deck[i][11].value = 'Q';
+        deck[i][11].suit = suits[i];
         deck[i][12].value = 'K';
+        deck[i][12].suit = suits[i];
     }
+
     return 0;
 }
 int checkCard(Card *deck_card) {
