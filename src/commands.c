2c38f991ecd8fffcfbff54560302df7852dfffae
mamm1504 <s215805@student.dtu.dk>
2022-04-22 11:26:15 +0200

    added file for commands

---
+++ src/commands.c
@@ -0,0 +1,9 @@
+//
+// Created by Mads Sørensen on 22/04/2022.
+//
+
+#include "commands.h"
+
+int LD() {
+
+}
---
+++ src/commands.h
@@ -0,0 +1,10 @@
+//
+// Created by Mads Sørensen on 22/04/2022.
+//
+
+#ifndef YUKON_COMMANDS_H
+#define YUKON_COMMANDS_H
+
+int LD();
+
+#endif //YUKON_COMMANDS_H
