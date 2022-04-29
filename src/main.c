222fbba4fae51475872b9ca60cd9c68fe63f5c96
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2022-04-29 09:33:25 +0200

    renamed input variables in loaddeck loop

--- src/main.c
+++ src/main.c
@@ -12,40 +12,40 @@ int main(void) {
     setbuf(stdout, 0);
 #endif
 
-    char command[256], filename[256], arg[256], buf[256];
-    Linked_list *LoadedDeck;
+    char command[256], filepath[256], arg[256], buf[256];
+    Linked_list *loadedDeck;
     bool deckLoaded = false;
     bool startupPhase = true, gameRunning = true;
     generateEmptyView("", "");
 
     while (!deckLoaded) {
         fgets(buf, sizeof(buf), stdin);
-       int numOfInputs = sscanf(buf, "%s %s", command, filename);
+        int numOfInputs = sscanf(buf, "%s %s", command, arg);
 
         if (strcasecmp("LD", command) == 0) {
-            /** If arg is not empty */
+            /** If filepath is not empty */
             if (numOfInputs == 2) {
-                strncpy(arg, "../resources/", 256);
-                strcat(arg, filename);
-                strcat(arg, ".txt");
+                strncpy(filepath, "../resources/", 256);
+                strcat(filepath, arg);
+                strcat(filepath, ".txt");
 
                 FILE *file = fopen(arg, "r");
 
                 /** If file exists */
                 if (file != NULL) {
-                    LoadedDeck = loadDeck(file);
-                    showDeck(LoadedDeck, false);
+                    loadedDeck = loadDeck(file);
+                    showDeck(loadedDeck, false);
                     deckLoaded = true;
                     fclose(file);
                 } else {
                     generateEmptyView("LD", "The file does not exist");
                 }
 
-            /** If no filename is given, load unshuffled deck */
+                /** If no filepath is given, load unshuffled deck */
             } else {
                 FILE *defaultDeck = fopen("../resources/default.txt", "r");
-                LoadedDeck = loadDeck(defaultDeck);
-                showDeck(LoadedDeck, false);
+                loadedDeck = loadDeck(defaultDeck);
+                showDeck(loadedDeck, false);
                 deckLoaded = true;
                 fclose(defaultDeck);
             }
@@ -61,7 +61,7 @@ int main(void) {
         int numOfInputs = sscanf(buf, "%s %s", command, arg);
 
         if (strcasecmp("SW", command) == 0) {
-            showDeck(LoadedDeck, true);
+            showDeck(loadedDeck, true);
         } else if (strcasecmp("SD", command) == 0) {
 
             /*strncpy(arg, "../resources/", 256);
@@ -69,22 +69,22 @@ int main(void) {
             strcat(arg, ".txt");*/
 
 
-            //SD(LoadedDeck, filepath);
+            //SD(loadedDeck, filepath);
 
         } else if (strcasecmp("SI", command) == 0) {
             int split;
             if (numOfInputs == 1) {
-                split = rand() % (LoadedDeck->size - 1) + 1;
+                split = rand() % (loadedDeck->size - 1) + 1;
             } else {
                 split = atoi(arg);
                 if (split == 0) {
                     generateEmptyView("SI", "ERROR! You can't split on something that ain't a number.");
-                } else if (split >= LoadedDeck->size) {
+                } else if (split >= loadedDeck->size) {
                     generateEmptyView("SI", "ERROR! You can't split on a number bigger than"
                                             " the number of cards in the deck.");
                 }
             }
-            LoadedDeck = SI(LoadedDeck, split);
+            loadedDeck = SI(loadedDeck, split);
         } else if (strcasecmp("QQ", command) == 0) {
             QQ(0, 0);
             break;
