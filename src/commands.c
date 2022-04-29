bb52d3fba8180b7caa7744e266941332ed8f8ecb
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2022-04-29 09:56:51 +0200

    removed qq because main calls exit after main

--- src/commands.c
+++ src/commands.c
@@ -98,12 +98,3 @@ Linked_list *SR(Linked_list *unshuffledPile) {
     }
     return 0;
 }
-
-void QQ(Linked_list *lists[], int numberOfLists) {
-    for (int i = 0; i < numberOfLists; i++) {
-        deleteLinkedList(lists[i]);
-    }
-
-    puts("Ending Yukon...");
-    exit(0);
-}
